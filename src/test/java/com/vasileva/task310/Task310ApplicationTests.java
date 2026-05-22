45a959f223aaea56ecd2c9fc787d7bd85f041ea9
KatiaVasileva <eka.vasileva@gmail.com>
2026-05-22 17:33:19 +0500

    initial commit

---
+++ src/test/java/com/vasileva/task310/Task310ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vasileva.task310;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Task310ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
